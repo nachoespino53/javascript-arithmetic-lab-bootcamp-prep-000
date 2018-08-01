84e9899be8b6b60ad798e3885d71a3a75005c63a
Ignacio R Espino <irespino@capozios.com>
2018-08-01 15:20:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const add = (a, b) => a + b;
-
 const subtract = (a, b) => a - b;
-
 const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const inc = a => a++;
+const dec = n => n--;
+const makeInt = n => {
+  
+}
+
+const preserveDecimal = n => {
+  
+}
