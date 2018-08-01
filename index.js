372f7d209efa53cd6f327117e5d0f0d1305cc0ce
Ignacio R Espino <irespino@capozios.com>
2018-08-01 15:22:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
-const inc = a => a + 1;
-const dec = n => n - 1;
+const inc = a => ++a;
+const dec = n => --n;
 const makeInt = n => {
   
   return parseInt(n);
